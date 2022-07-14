df6fe35538cd827480c9b53c83ca01eaa8d9f012
MisevrO <108984302+MisevrO@users.noreply.github.com>
2022-07-14 11:03:54 +0300

    Add files via upload

---
+++ lesson 3.py
@@ -0,0 +1,34 @@
+# lesson 3
+# Зформуйте строку, яка містить певну інформацію про символ в відомому слові.
+# Наприклад "The [номер символу] symbol in [тут слово] is '[символ з відповідним порядковим номером]'".
+# Слово та номер отримайте за допомогою input() або скористайтеся константою.
+# Наприклад (слово - "Python" а номер символу 3) - "The 3 symbol in "Python" is 't' ".
+
+number = 3
+word = 'Python'
+serial_number = word[2]
+result = f"The {number} symbol in {word} is {serial_number}".format()
+print(result)
+
+
+# Вести з консолі строку зі слів за допомогою input() (або скористайтеся константою).
+# Напишіть код, який визначить кількість слів, в цих даних.
+
+tpl = "The 3 symbol in Python is 't'"
+result = len(tpl)
+print(result)
+
+# Існує ліст з різними даними, наприклад lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'].
+# Напишіть код, який сфлрмує новий list (наприклад lst2), який би містив всі числові змінні (int, float), які є в lst1.
+# Майте на увазі, що данні в lst1 не є статичними можуть змінюватись від запуску до запуску.
+
+lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
+lst1.remove(True)
+
+lst2 = []
+for elem in lst1:
+    if isinstance(elem, (int, float)):
+        lst2.append(elem)
+print(lst2)
+
+
